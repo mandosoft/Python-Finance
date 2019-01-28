897d63561a58fb2194c56db048d9a1e26fc07c3c
Thomas Townsley <thomasdtownsley@gmail.com>
2019-01-28 14:57:19 -0600

    added libraries and imported json file

--- vix_json.py
+++ vix_json.py
@@ -1,11 +1,28 @@
+"""
+K-Means clustering applied to time series data. In this project 
+CBOE VIX data from 2004 to the Present is used. 
+
+Sentinels (#%%) at the begining are for running python files as Jupyter notebook cells 
+in VSCode.
+
+"""
+
+#%%
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
 import json
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+#%%
 
-with open('vix-daily_json.json', 'r') as vix:
+with open('/Users/Thomas/Desktop/Finance_Python/vix-daily_json.json', 'r') as vix:
     data=vix.read()
-
 obj = json.loads(data)
-
-print(obj['High'])
+print(obj[])
 
 
 
